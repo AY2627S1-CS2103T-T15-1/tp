8ecb233b2eaa3852dbd26c157004505d48741060
Ling Song <lingsong.c4@gmail.com>
2026-09-17 21:57:50 +0800

    Add RemarkCommandTest

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -8,6 +8,7 @@ import seedu.address.model.Model;
 public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
 
     @Override
     public CommandResult execute(Model model) {
